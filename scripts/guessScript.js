454ba8c9a1d7ffdccf7f998c69934e36097c0be4
Hack Reactor Students <accounts@hackreactor.com>
2014-08-26 17:22:53 -0700

    asdasd

--- scripts/guessScript.js
+++ scripts/guessScript.js
@@ -76,7 +76,7 @@ put in profile
       $scope.togglerPop = false;
       $scope.$apply();
       console.log('triggered');
-    }, 1000);
+    }, 930);
 
 
     }
@@ -95,9 +95,9 @@ put in profile
 
 
     console.log(percentageSecond);
-    $scope.personalCounter++;
-$scope.stack(percentageFirst, percentageSecond);
 
+$scope.stack(percentageFirst, percentageSecond);
+  $scope.personalCounter++;
   };
 //BAR HERE-------------------------------------------------------
     $scope.max = 200;
@@ -118,9 +118,52 @@ $scope.stack(percentageFirst, percentageSecond);
 
 //BAR HERE ----------------------------------------------------------------
 
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+
+
 $scope.initialize = function()
 {
    $scope.setBar();
+   var repeat = function(){
+
+  var temp = Math.round(getRandomArbitrary(1,2));
+  console.log(temp);
+
+  if(temp === 1)
+  {
+      $scope.displayFirst = Counter.increment($scope.displayFirst);
+  }
+  else
+  {
+      $scope.displaySecond = Counter.increment($scope.displaySecond);
+  }
+
+    var percentageFirst = 0;
+    var percentageSecond = 0;
+
+    var totalPops = $scope.displayFirst + $scope.displaySecond;
+
+    percentageFirst = Math.round(($scope.displayFirst/totalPops) *100);
+    console.log(percentageFirst);
+    percentageSecond = Math.round(($scope.displaySecond/totalPops) *100);
+
+
+    console.log(percentageSecond);
+
+$scope.stack(percentageFirst, percentageSecond);
+
+
+
+
+   $timeout(function(){
+    repeat();
+   }, getRandomArbitrary(100,1300));
+ }
+ repeat();
+
 }
 
 
